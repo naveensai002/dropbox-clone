ceb05f4f7aafb2553bc13887ebcf4c8dda91f614
naveensai <sainaveen9000@gmail.com>
2023-12-01 20:32:42 +0530

    second-commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 
 import { FaArrowRightLong } from "react-icons/fa6";
+import Link  from 'next/link';
 
 
 export default function Home() {
 	return (
-		<header className='pt-8 flex flex-col items-center leading-8  bg-[#1f1819] text-[#f7f5f2] p-12'>
+		<header className='pt-8 flex flex-col items-center leading-8  bg-[#1f1819] text-[#f7f5f2] p-18 pb-12'>
 			<div className='leading-8 w-3/4 justify-center flex items-center flex-col gap-y-4 '>
 				<h1 className='tracking-widest text-3xl'>Welcome to the Dropbox</h1>
 				<h2 className='text-2xl tracking-widest '>
@@ -15,12 +16,24 @@ export default function Home() {
 				<p className='tracking-tight pb-12'>
 					With Dropbox, you get a full suite of tools designed to help you
 					create, share, manage, and track content <br />
-					 more efficiently. Plus, proven cloud storage you can trust.
+					more efficiently. Plus, proven cloud storage you can trust.
 				</p>
-				<button className='btn btn-lg btn-primary tracking-widest bg-[#3984ff]  pb-6'>Get started now
-				<span>
-					<FaArrowRightLong />
-					</span> </button>
+				
+					<Link href='/dashboard' className='btn btn-lg btn-primary tracking-widest bg-[#3984ff] pt-6 pb-12'>
+						Get started now
+						<span>
+							<FaArrowRightLong />
+						</span>
+					</Link>
+				
+
+				{/* video */}
+				<video muted autoPlay loop className='rounded-lg'>
+					<source
+						src='https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/dropbox/dbx1-hero-1920x1080.mp4'
+						type='video/mp4'
+					/>
+				</video>
 			</div>
 		</header>
 	);
